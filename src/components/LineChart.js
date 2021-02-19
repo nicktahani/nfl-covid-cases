2fa8a026aa599097119dcb5ded58e57ff613c631
Nick t <ns.tahani@gmail.com>
2021-02-19 14:39:01 -0800

    finished LineChart; TODO clean it up

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,10 +1,15 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useRef, useEffect } from 'react'
 import { extent } from 'd3-array'
 import { scaleLinear, scalePoint } from 'd3-scale'
 import { line, curveMonotoneX } from 'd3-shape'
 import { nest } from 'd3-collection'
+import { select } from 'd3-selection'
+import { axisLeft, axisBottom } from 'd3-axis'
 
-function LineChart ({ width = 900, height = 600, data }) {
+function LineChart ({ width = 900, height = 700, data }) {
+  const svg = useRef()
+  const margin = {top: 20, right: 30, bottom: 30, left: 40}
+
   const nestedData = nest() 
     .key(d => d.week)
     .rollup(d => d.length)
@@ -12,16 +17,33 @@ function LineChart ({ width = 900, height = 600, data }) {
 
     console.log(nestedData)
 
+  useEffect(() => {
+    if (!svg.current) return;
+    
+    const svgNode = select(svg.current)
+    const XaxisGenerator = axisBottom(xScale)
+    const YaxisGenerator = axisLeft(yScale)
+    
+    svgNode.append('g')
+      .attr("transform", `translate(0, ${height - margin.bottom})`)
+      .call(XaxisGenerator)
+    
+    svgNode.append('g')
+      .attr("transform", `translate(${margin.left}, 0)`)
+      .call(YaxisGenerator)
+
+  }, [])
+
   const xScale = useMemo(() => 
     scalePoint()
       .domain(nestedData.map(d => d.key))
-      .range([0, width]), 
+      .range([margin.left, width - margin.right]), 
       [nestedData]
   )
   const yScale = useMemo(() => 
     scaleLinear()
       .domain(extent(nestedData, d => d.value))
-      .range([height, 0]), 
+      .range([height - margin.bottom, margin.top]), 
       [nestedData]
   )
   const path = useMemo(() => 
@@ -32,10 +54,11 @@ function LineChart ({ width = 900, height = 600, data }) {
     [xScale, yScale]
   )
   return (
-    <svg width={width} height={height}>
+    <svg ref={svg} width={width} height={height}>
       <path 
-        style={{ fill: "none", stroke: 'steelblue', strokeWidth: '2' }} //clean up
-        d={path(nestedData)} />
+        d={path(nestedData)}
+        style={{ fill: 'none', stroke: 'steelblue', strokeWidth: '2' }} //clean up 
+      />
     </svg>
   )
 }
