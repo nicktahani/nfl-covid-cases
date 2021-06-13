db3cf3f713bdd12e66401c6cc0bff2fc9803f574
Nick t <ns.tahani@gmail.com>
2021-06-13 13:17:14 -0700

    generalize InfoBox

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className='App' style={{ margin: '40px' }}>  
       <span className='header'>NFL COVID CASES</span>
-      <InfoBox />
+      <InfoBox description='The NFL has created a COVID-19 Reserve List for the 2020-21 season that is defined as any player who has tested positive, or been in close contact with someone who has tested positive...'/>
       <FetchCovidData />
     </div>
   );
--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export function InfoBox() {
+export function InfoBox({description}) {
   return (
     <>
       <p>
-        The NFL has created a <i>COVID-19 Reserve List</i> for the 2020-21 season that is defined as any player who has tested positive, or been in close contact with someone who has tested positive...
+        {description}
       </p>
     </>
   )
