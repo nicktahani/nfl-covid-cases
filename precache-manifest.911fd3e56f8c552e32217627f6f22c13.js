6124b3f8140f04912d9f67df7c802db3c76fcce9
Nick t <ns.tahani@gmail.com>
2021-07-29 17:01:02 -0700

    Updates

---
+++ precache-manifest.911fd3e56f8c552e32217627f6f22c13.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19d994eb63611ea55bb7daf858d8845",
+    "url": "/nfl-covid-cases/index.html"
+  },
+  {
+    "revision": "02dbb06e63384f75f601",
+    "url": "/nfl-covid-cases/static/css/main.45f1f38d.chunk.css"
+  },
+  {
+    "revision": "256f51235bf933afbc4d",
+    "url": "/nfl-covid-cases/static/js/2.a57179fa.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/nfl-covid-cases/static/js/2.a57179fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02dbb06e63384f75f601",
+    "url": "/nfl-covid-cases/static/js/main.1603cf1a.chunk.js"
+  },
+  {
+    "revision": "7829b5a54f29b16f88eb",
+    "url": "/nfl-covid-cases/static/js/runtime-main.9f4574e9.js"
+  },
+  {
+    "revision": "301fe70f8f0f41c236317504ec05f820",
+    "url": "/nfl-covid-cases/static/media/Roboto-Black.301fe70f.ttf"
+  },
+  {
+    "revision": "f36638c2135b71e5a623dca52b611173",
+    "url": "/nfl-covid-cases/static/media/Roboto-Regular.f36638c2.ttf"
+  },
+  {
+    "revision": "4e4ddc8c402295893fb881d4bcc43fad",
+    "url": "/nfl-covid-cases/static/media/UnicaOne-Regular.4e4ddc8c.ttf"
+  }
+]);
